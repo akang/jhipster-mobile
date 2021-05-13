c88e66186200460b9a008df161659a3073b0a073
Allan Kenneth Ang <allkenang@gmail.com>
2021-05-13 08:57:40 +0800

    added axios

--- src/redux/authentication.reducer.ts
+++ src/redux/authentication.reducer.ts
@@ -1,4 +1,5 @@
 import {IAccount} from '../models/account.model';
+import axios from 'axios';
 
 
 const initialState = {
@@ -8,7 +9,8 @@ const initialState = {
 };
 
 export const ACTION_TYPES = {
-    LOGIN:'auth/LOGIN'
+    LOGIN:'authentication/LOGIN',
+    GET_SESSION: 'authentication/GET_SESSION',
 };
 
 export type AuthenticationState = Readonly<typeof initialState>;
@@ -27,11 +29,30 @@ export default (state:AuthenticationState = initialState, action: any):Authentic
     }
 }
 
-export const login = (username:string, password:string) => {
-    // TODO implement login
-    console.log('logging in')
-    return {
+
+export const getSession: () => void = () => (dispatch, getState) => {
+    dispatch({
+        type: ACTION_TYPES.GET_SESSION,
+        payload: axios.get('api/account'),
+    });
+};
+
+export const login: (username: string, password: string, rememberMe?: boolean) => void = (username, password, rememberMe = true) => async (
+    dispatch,
+    getState
+) => {
+    const result = await dispatch({
         type: ACTION_TYPES.LOGIN,
-        payload: '1'
+        payload: axios.post('api/authenticate', { username, password, rememberMe }),
+    });
+    const bearerToken = result.value.headers.authorization;
+    if (bearerToken && bearerToken.slice(0, 7) === 'Bearer ') {
+        const jwt = bearerToken.slice(7, bearerToken.length);
+        // if (rememberMe) {
+        //     Storage.local.set(AUTH_TOKEN_KEY, jwt);
+        // } else {
+        //     Storage.session.set(AUTH_TOKEN_KEY, jwt);
+        // }
     }
-}
+    await dispatch(getSession());
+};
